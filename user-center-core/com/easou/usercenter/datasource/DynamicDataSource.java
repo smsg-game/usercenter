8d655d57b305a4cda14e85fbdf1078392981f6ca
dogdog7788 <dogdog7788@qq.com>
2015-12-08 15:38:57 +0800

    no message

--- user-center-core/com/easou/usercenter/datasource/DynamicDataSource.java
+++ user-center-core/com/easou/usercenter/datasource/DynamicDataSource.java
@@ -1,5 +1,8 @@
 package com.easou.usercenter.datasource;
 
+import java.sql.SQLFeatureNotSupportedException;
+import java.util.logging.Logger;
+
 import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
 
 public class DynamicDataSource extends AbstractRoutingDataSource {
@@ -8,6 +11,12 @@ public class DynamicDataSource extends AbstractRoutingDataSource {
 	protected Object determineCurrentLookupKey() {
 		// TODO Auto-generated method stub
 		return ContextHolder.getContext(); 
+	}
+
+	@Override
+	public Logger getParentLogger() throws SQLFeatureNotSupportedException {
+		// TODO Auto-generated method stub
+		return null;
 	} 
 
 }
